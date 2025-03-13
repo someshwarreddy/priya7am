9fa00e90fb42fccff39ace7eb14b58b43760d8dc
someshwar reddy <somesh.satish@gmail.com>
2025-03-13 08:04:52 +0530

    datatypes

--- javascript/datatypes.js
+++ javascript/datatypes.js
@@ -74,11 +74,69 @@ console.log(c == d); // here values compared
 
 
 console.log(typeof Symbol() === "symbol");
-console.log (typeof Symbol("foo") === "symbol");
+console.log(typeof Symbol("foo") === "symbol");
 console.log(typeof Symbol.iterator === "symbol")
 
 const sym1 = Symbol();
-const sym2 = Symbol("foo");
-const sym3 = Symbol("foo");
+const sym2 = Symbol("foo"); // unique 
+const sym3 = Symbol("foo"); // unique
 
 console.log(sym2 == sym3)
+
+const uid1 = Symbol.for('hi'); // same refernce 
+const uid2 = Symbol.for('hi'); // same reference
+
+console.log(uid1 == uid2);
+
+const obj1 = {
+    [Symbol('uid')]: 'uid',
+}
+
+// if(obj1.Symbol('uid') == obj1.Symbol('uid')){
+//     alert('uid')
+// }
+
+
+console.log(2 + 2); // Number + Number
+
+console.log('2' + 2); //4 or 22 or '2'2
+
+console.log(+"2" + 2); //here keeping + symbol before string which convers the string to Number
+
+console.log('2' == 2); // true because here number 2 which is converted as string 
+
+// which means double equal to compares the values 
+
+console.log('2' === 2); // here ===  is checks the type of the value  string === number so false
+
+var abc = '2';
+var bac = 2;
+console.log(abc == bac);
+console.log(abc === bac);
+
+var abo = [];
+var bao = {};
+
+console.log(abo == bao); // by reference 
+console.log(abo === bao); // by 
+
+var m = Number("1");
+var p = 2;
+var mp = m / p;
+console.log(typeof (mp), mp); // number 
+
+// NaN 
+
+console.log(Number('kkk') + 2);
+console.log('string' / 2);
+console.log(undefined + 2);
+console.log(null + 2); // null which is a value 
+
+
+console.log(isNaN(mp));
+
+if(isNaN(mp)) {
+    alert('please check the values which are provided')
+}
+
+
