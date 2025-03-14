8159b29816187750eea68bf6b627a3bf4b3269ea
someshwar reddy <somesh.satish@gmail.com>
2025-03-14 07:56:55 +0530

    variables

---
+++ javascript/variables.js
@@ -0,0 +1,25 @@
+// we have three types var , let, const
+
+console.log(newone);
+var aName = 10;
+console.log(aName)
+// defult value is undefined untill username is intilize for var key word
+
+var aName = 10;
+
+console.log(aName);
+
+var newone;
+
+
+// console.log(iamlet); // reference error : canot access iamlet it value does not intilized
+let iamlet = 100;
+console.log(iamlet);
+// let does not consist default value that is undefined 
+// let does not reside inside a global window object 
+// let reside in a seprate script
+
+const iamconst = '100'
+
+
+
